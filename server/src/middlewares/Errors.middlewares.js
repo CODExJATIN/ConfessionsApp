1d167db5aec34ff867894f229283f9449bb1167b
kartik <karambelkarkartik209@gmail.com>
2025-06-05 14:27:09 +0530

    bug fix

--- server/src/middlewares/Errors.middlewares.js
+++ server/src/middlewares/Errors.middlewares.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
-import { ApiError } from "../utils/apiError";
+import { ApiError } from "../utils/apiError.js";
 
 
 
 const Error_Handler=(err,req,res,next)=>{
     let error =err
     if (!(error instanceof ApiError)) {
-        const statusCode= error.statusCode || error instanceof mongoose.Error ? 400 :500
+       const statusCode = error.statusCode ? error.statusCode : (error instanceof mongoose.Error ? 400 : 500);
+
       
         const message=error.message || "something went wrong"
      error = new ApiError(statusCode, message ,error?.errors || [], error.stack)
